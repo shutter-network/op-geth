d71f89787f43892bb24e2f1aeaf59ce4bf00251e
Jannik Luhn <jannik.luhn@posteo.de>
2023-10-30 21:38:04 +0300

    Add function to fetch submitted txs from inbox

--- core/shutter.go
+++ core/shutter.go
@@ -14,6 +14,12 @@ var ShutterSystemAddress = common.HexToAddress("0x800000000000000000000000000000
 
 var ErrNoActiveKeyperSet = errors.New("no active keyper set at current block number")
 
+type EncryptedTransaction struct {
+	EncryptedTransaction []uint8
+	GasLimit             uint64
+	CumulativeGasLimit   uint64
+}
+
 // AreShutterContractsDeployed checks if the system contracts required for
 // Shutter to operate are deployed.
 func AreShutterContractsDeployed(evm *vm.EVM) bool {
@@ -180,6 +186,52 @@ func IsShutterEnabled(evm *vm.EVM) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func GetSubmittedEncryptedTransactions(evm *vm.EVM, blockNumber uint64) ([]EncryptedTransaction, error) {
+	data, err := shutter.InboxABI.Pack("getTransactions", blockNumber)
+	if err != nil {
+		return []EncryptedTransaction{}, err
+	}
+	sender := vm.AccountRef(common.Address{})
+	ret, _, err := evm.Call(
+		sender,
+		evm.ChainConfig().Shutter.InboxAddress,
+		data,
+		100_000_000,
+		new(big.Int),
+	)
+	if err != nil {
+		return []EncryptedTransaction{}, err
+	}
+
+	unpacked, err := shutter.InboxABI.Unpack("getTransactions", ret)
+	if err != nil {
+		return []EncryptedTransaction{}, err
+	}
+	if len(unpacked) != 1 {
+		return []EncryptedTransaction{}, fmt.Errorf("inbox returned unexpected number of values")
+	}
+
+	anonTxs, ok := unpacked[0].([]struct {
+		EncryptedTransaction []uint8 "json:\"encryptedTransaction\""
+		GasLimit             uint64  "json:\"gasLimit\""
+		CumulativeGasLimit   uint64  "json:\"cumulativeGasLimit\""
+	})
+	if !ok {
+		return []EncryptedTransaction{}, fmt.Errorf("inbox returned unexpected type")
+	}
+
+	txs := []EncryptedTransaction{}
+	for _, anonTx := range anonTxs {
+		tx := EncryptedTransaction{
+			EncryptedTransaction: anonTx.EncryptedTransaction,
+			GasLimit:             anonTx.GasLimit,
+			CumulativeGasLimit:   anonTx.CumulativeGasLimit,
+		}
+		txs = append(txs, tx)
+	}
+	return txs, nil
 }
 
 func ApplyRevealMessage(evm *vm.EVM, msg *Message, gp *GasPool) (*ExecutionResult, error) {
